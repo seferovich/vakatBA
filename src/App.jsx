e5bfa7e0a6ff90e83e088aa4ee096e0606309180
sefkelol <seferovicisa@gmail.com>
2022-09-19 22:32:12 +0200

    Added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import {Routes, Route} from 'react-router-dom'
 import Cities from "./components/Cities";
 import Home from './components/Home'
 
 function App() {
   const [data, setData] = useState('Loading...')
-  const [city, setCity] = useState([])
+  const [city, setCity] = useState([]) 
   const [darkMode, setDarkMode] = useState(false)
-
   
-
+  
+  
   // API fetches for data and cities
 
   const fetchData = async (id) => {
@@ -23,24 +23,49 @@ function App() {
     .then(res => res.json())
     .then(data => setCity(data))
   }
-
-
+  
+  
   useEffect(()=>{
-    fetchData(16)
+    // Get location from LS
+    if(localStorage.location > 1){
+      fetchData(JSON.parse(localStorage.getItem('location')))
+    }else{
+      fetchData(77)
+    }
+
     fetchCity()
-  },[])
+
+    
+    // Get theme from LS
+    if(localStorage.length > 0){
+      setDarkMode(JSON.parse(localStorage.getItem('darkMode')))
+    }else{
+      localStorage.setItem('darkMode', darkMode)
+    }
+  }, [])
+
+  // Track theme changes in LS
+  useEffect(() => {
+    setDarkMode(JSON.parse(localStorage.getItem('darkMode')))
+  }, [darkMode])
+ 
  
 
   // Fetching for cities
   const handleFetch = (e) => {
     fetchData(e.target.id)
+    localStorage.setItem('location', e.target.id)
   }
 
+  
   // Switching dark/light themes
   const handleChange = (e) => {
-    setDarkMode(prevDarkMode => !prevDarkMode)
+    setDarkMode(prevDarkMode => localStorage.setItem('darkMode', !prevDarkMode))
+    
     e.preventDefault()
   }
+
+  
 
   return (
     <div className={darkMode ? 'app-dark' : 'app'}>    
@@ -63,9 +88,7 @@ function App() {
         />} 
         />
 
-      </Routes>
-      
-        
+      </Routes>   
     </div>
     
   );
